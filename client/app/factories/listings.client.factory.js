c37bbccbf780cb035340ee9de3bfe1ebe62638c3
Conor Landry <clandry94@ufl.edu>
2017-09-16 18:00:37 -0400

    almost done

--- client/app/factories/listings.client.factory.js
+++ client/app/factories/listings.client.factory.js
@@ -1,21 +1,25 @@
-angular.module('listings').factory('Listings', ['$http', 
+angular.module('listings').factory('Listings', ['$http',
   function($http) {
     var methods = {
       getAll: function() {
         return $http.get('http://localhost:8080/api/listings');
       },
 
+      edit: function(id) {
+        return $http.get('http://localhost:8080/listings/' + id + 'edit');
+      },
+
       create: function(listing) {
         return $http.post('http://localhost:8080/api/listings', listing);
-      }, 
+      },
 
       read: function(id) {
         return $http.get('http://localhost:8080/api/listings/' + id);
-      }, 
+      },
 
       update: function(id, listing) {
         return $http.put('http://localhost:8080/api/listings/' + id, listing);
-      }, 
+      },
 
       delete: function(id) {
         return $http.delete('http://localhost:8080/api/listings/' + id);
